806e8ff2e249d8d008c4821242db6b70e94ceabe
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-06-09 18:46:52 -0400

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -2,24 +2,29 @@ import React from 'react'
 
 class Pet extends React.Component {
   render() {
+    // console.log("Pet's props:", this.props.pet)
+    let {pet: {name, gender, type, age, weight, isAdopted, id}, handleAdoptPet} = this.props
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+          {gender === "female" ? '♀ ' : '♂ '}
+            {name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+        {
+          isAdopted === true ? <button className="ui disabled button">Already adopted</button> : <button className="ui primary button"
+          data-id={`${id}`}
+          onClick={() => handleAdoptPet(id)}>Adopt pet</button>
+        }
         </div>
       </div>
     )
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,7 +4,16 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    // {console.log(this.props.listOfPets)}
+    let pets = this.props.listOfPets
+    return (<div className="ui cards">
+      {pets.map(petObj => <Pet
+        key={petObj.key}
+        pet={petObj}
+        handleAdoptPet={this.props.handleAdoptPet}
+        /> )}
+    </div>
+  )
   }
 }
 
